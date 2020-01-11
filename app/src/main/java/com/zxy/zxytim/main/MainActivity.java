52a4c7de704e1fb3718e3629609f4b1309b33655
张行于 <659966631@qq.com>
2020-01-11 17:04:47 +0800

    增加登出+消息未读数量

--- app/src/main/java/com/zxy/zxytim/main/MainActivity.java
+++ app/src/main/java/com/zxy/zxytim/main/MainActivity.java
@@ -3,23 +3,32 @@ package com.zxy.zxytim.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.tencent.imsdk.TIMConversation;
+import com.tencent.imsdk.TIMConversationType;
+import com.tencent.imsdk.TIMManager;
 import com.zxy.zxyim.ARoutPath;
 import com.zxy.zxyim.ZxyTimUtls;
 import com.zxy.zxytim.R;
 
 
 @Route(path = ARoutPath.MAIN)
-public class MainActivity extends AppCompatActivity   {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
         ZxyTimUtls.getInstance().login("207026", "eJyrVgrxCdZLrSjILEpVsjIzMLEwMNABi5WlFilZKRnpGShB*MUp2YkFBZkpSlaGJgYGRpbGRuamEJnMlNS8ksy0TIgGA3MDIzOYnsx0oFBGsm9hUHZKaUl4UaC*o7*-b1JGpEd5upF-TomJT1JVgHahh4FTVYFJfmS6LVRjSWYu0DmGpuYW5oYmZgYGtQDpzjDT");
+
+
+
         findViewById(R.id.singnchat).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- zxyim/src/main/java/com/zxy/zxyim/ZxyTimUtls.java
+++ zxyim/src/main/java/com/zxy/zxyim/ZxyTimUtls.java
@@ -3,6 +3,8 @@ package com.zxy.zxyim;
 import android.util.Log;
 
 import com.tencent.imsdk.TIMCallBack;
+import com.tencent.imsdk.TIMConversation;
+import com.tencent.imsdk.TIMConversationType;
 import com.tencent.imsdk.TIMManager;
 
 public class ZxyTimUtls {
@@ -42,6 +44,50 @@ public class ZxyTimUtls {
             }
 
         });
+    }
+
+    /**
+     * 登出
+     */
+    public void loginOut() {
+        //登出
+        TIMManager.getInstance().logout(new TIMCallBack() {
+            @Override
+            public void onError(int code, String desc) {
+                //错误码 code 和错误描述 desc，可用于定位请求失败原因
+                //错误码 code 列表请参见错误码表
+                Log.d(tag, "logout failed. code: " + code + " errmsg: " + desc);
+            }
+
+            @Override
+            public void onSuccess() {
+                //登出成功
+            }
+        });
+    }
+
+
+    /**
+     * 获取会话扩展实例
+     *
+     * @param conversationId
+     */
+    private TIMConversation getTIMConversation(String conversationId) {
+        //获取会话扩展实例
+        TIMConversation con = TIMManager.getInstance().getConversation(TIMConversationType.C2C, "207026");
+        return con;
+    }
+
+    /**
+     * 获取会话未读数
+     *
+     * @param conversationId
+     */
+    public long getUnreadMessageNum(String conversationId) {
+        //获取会话未读数
+        long num = getTIMConversation(conversationId).getUnreadMessageNum();
+        Log.e("zxyIM", "unread msg num: " + num);
+        return num;
     }
 
 }
